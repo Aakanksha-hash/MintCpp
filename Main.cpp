692b7a3245f2b7d0272344cfae0f57bb4f620d13
Aakanksha-hash <poojaryaakanksha@gmail.com>
2019-10-01 20:22:07 +0530

    Added the add function

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 typedef std::string Mint;
 
+Mint add(Mint a, Mint b)
+{
+	string ans="";
+	int len1 = a.length(), len2 = b.length(), sum=0, carry=0, j=len1-1;
+	
+	while(len1--, len2--)
+	{
+		sum = int(a[len1])+int(b[len2])-96+carry;
+		ans+=sum%10+48;
+		carry = sum/10;
+	}
+	len1++;
+	 while(len1--)
+	 {
+		sum = int(a[len1])-48+carry;
+		ans+=sum%10+48;
+		carry = sum/10;
+	 }
+	 if(carry)
+	 ans+=carry+48;
+	return string(ans.rbegin(), ans.rend());
+}
+
 int main()
 {
 	Mint str = "98";
