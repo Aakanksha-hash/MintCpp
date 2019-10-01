c213a90c498783855fc171a47cee8671a2497b04
Aakanksha-hash <poojaryaakanksha@gmail.com>
2019-10-01 20:05:09 +0530

    Added todos

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 typedef std::string Mint;
+
 int main()
 {
 	Mint str = "98";
+
 	std::cout << str << '\n';
 }
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # MintCpp
 A library for Big Integers in C++.
+
+To do:  
+Space separated input support
+Addition
+Subtraction
+
+
