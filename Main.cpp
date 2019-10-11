e27197ee4ea72e0d5d9ada8ab55e2932d448fac2
Surya Madhavan <32214378+greykoalacode@users.noreply.github.com>
2019-10-11 22:06:34 +0530

    Updated with Mini Function
    Mini function basically returns string/integer of lesser value out of the two

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
+#include<math.h>
 typedef std::string Mint;
-
 Mint add(Mint a, Mint b)
 {
-	string ans="";
+	std::string ans="";
 	int len1 = a.length(), len2 = b.length(), sum=0, carry=0, j=len1-1;
 	if(len1 < len2)
 	{
 		a.swap(b);
-		swap(len1, len2);
+		std::swap(len1, len2);
 	}
 	while(len1--, len2--)
 	{
@@ -25,17 +25,37 @@ Mint add(Mint a, Mint b)
 	 }
 	 if(carry)
 	 ans+=carry+48;
-	return string(ans.rbegin(), ans.rend());
+	return std::string(ans.rbegin(), ans.rend());
+}
+Mint mini(Mint a,Mint b)
+{
+	Mint ans="";
+	
+	for(int i = 0; i < a.length(); ++i)
+	{
+    if ((a[i] - '0') < (b[i] - '0'))
+    { 
+        ans=a;
+    }
+    else if((a[i]-'0') =='0' || (b[i]-'0')=='0')
+    {
+    	continue;
+	}
+    else
+    ans=b;
+    
+    }
+	return ans;
 }
 
-Mint sub(string a, string b)
+Mint sub(Mint a, Mint b)
 {
-	string ans="";
+	std::string ans="";
 	int len1 = a.length(), len2 = b.length(), sum=0, carry=0, j=len1-1, neg=0;
 	if(len1 < len2)
 	{
 		a.swap(b);
-		swap(len1, len2);
+		std::swap(len1, len2);
 		neg=1;
 	}
 	
@@ -58,12 +78,14 @@ Mint sub(string a, string b)
 	 }
 	 if(neg)
 		 ans+='-';
-	return string(ans.rbegin(), ans.rend());
+	return std::string(ans.rbegin(), ans.rend());
 }
 
 int main()
 {
-	Mint str = "98";
-
-	std::cout << str << '\n';
+	Mint str ;
+    Mint str1;
+    std::cin>>str>>str1;
+    Mint Res=mini(str,str1);
+	std::cout <<Res<< '\n';
 }
